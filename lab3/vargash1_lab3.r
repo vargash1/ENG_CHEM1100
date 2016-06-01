b90744a3134d7b7fdadf990378a4adacda91ddb5
Hector Vargas <vargash1@wit.edu>
2016-06-01 13:04:19 -0400

    Last changes hopefully

--- lab3/vargash1_lab3.r
+++ lab3/vargash1_lab3.r
@@ -3,10 +3,9 @@
 # @Name:   Vargas, Hector
 # @Email:  vargash1@wit.edu
 
-# Read in data, attach it for ease of use
-neon_data <- read.csv("neon.csv")
-hydrogen_data <- read.csv("hydrogen.csv")
-redLED_data <- read.csv("redled.csv")
+neon_data <- read.csv("neon.csv", stringsAsFactors=FALSE)
+hydrogen_data <- read.csv("hydrogen.csv", stringsAsFactors=FALSE)
+redLED_data <- read.csv("redled.csv", stringsAsFactors=FALSE)
 neon_zero <- 17.90
 hydrogen_zero <- 17.75
 redLED_zero <- 5.00
@@ -32,10 +31,16 @@ get_data <- function(element_data, element_zero, element_color){
     for(i in 1:length(element_data$Theta)){
         t_value <- element_data$Theta[i]
         n_value <- element_data$n[i]
+        o_color <- element_data$Color[i]
         lambda_value <- lab_lambda(t_value, n_value, element_zero)
         energy_level <- photon_enrgy(lambda_value)
-        cat(element_color, " Value for Theta:\t",t_value,
-            "\tEnergy Level(n): ",n_value, "\tE: ", energy_level, "\tLambda: ", lambda_value,"\n")
+        cat(element_color,
+            " Theta:\t",t_value,
+            "\tn): ",n_value,
+            "\tE: ", energy_level,
+            "\tLambda: ", lambda_value,
+            "\t Color:", o_color,
+            "\n")
     }
 }
 get_data(neon_data, neon_zero, "Neon")
